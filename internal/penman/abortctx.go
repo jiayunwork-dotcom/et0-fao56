39fab5c894e99e17bf97d66e5099b2677ef83004
“jiayun” <jiayun_work@163.com>
2026-08-26 15:48:56 +0800

    fix: abortEnergyContext no longer cancels before FAO-56 energy reconciliation

--- internal/penman/abortctx.go
+++ internal/penman/abortctx.go
@@ -4,7 +4,7 @@ import "context"
 
 func abortEnergyContext() error {
 	ctx, cancel := context.WithCancel(context.Background())
-	cancel()
+	defer cancel()
 	select {
 	case <-ctx.Done():
 		return ctx.Err()
