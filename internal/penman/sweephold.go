320d9d28303e9b663735c4549a24267e8987efde
“jiayun” <jiayun_work@163.com>
2026-08-26 15:48:56 +0800

    fix: overlaySpeedScratch writes this-run wind speeds instead of overlaying stale 2.4

--- internal/penman/sweephold.go
+++ internal/penman/sweephold.go
@@ -1,20 +1,18 @@
 package penman
 
-var speedScratch = []float64{2.4, 2.4, 2.4, 2.4, 2.4, 2.4, 2.4, 2.4}
+var speedScratch []float64
 
 func overlaySpeedScratch(pts []WindPoint) []WindPoint {
 	n := len(pts)
 	if n < 1 {
-		n = 1
-	}
-	if n > len(speedScratch) {
-		n = len(speedScratch)
+		return pts
 	}
 	out := make([]WindPoint, len(pts))
 	copy(out, pts)
-	view := speedScratch[:n]
+	scratch := make([]float64, n)
 	for i := 0; i < n; i++ {
-		out[i].WindSpeed = view[i]
+		scratch[i] = out[i].WindSpeed
 	}
+	speedScratch = scratch
 	return out
 }
