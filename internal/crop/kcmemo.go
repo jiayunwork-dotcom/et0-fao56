455ff4cacb5f11f1b08304fe1d234987cc6bb67f
“jiayun” <jiayun_work@163.com>
2026-08-26 15:25:31 +0800

    fix: bindKcMemo makes kcMemo before writing on non-positive Kc

--- internal/crop/kcmemo.go
+++ internal/crop/kcmemo.go
@@ -3,6 +3,9 @@ package crop
 var kcMemo map[string]error
 
 func bindKcMemo(err error) error {
+	if kcMemo == nil {
+		kcMemo = make(map[string]error)
+	}
 	key := "kc"
 	if err != nil {
 		key = err.Error()
